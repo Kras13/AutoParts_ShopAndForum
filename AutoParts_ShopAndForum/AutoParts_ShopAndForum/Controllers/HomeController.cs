ca43ad95c19b0bc3a16baef8c7361885221433c7
Kras13 <paskalev.1989@abv.bg>
2024-11-02 13:40:38 +0200

    Initial commit.

---
+++ AutoParts_ShopAndForum/AutoParts_ShopAndForum/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using AutoParts_ShopAndForum.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AutoParts_ShopAndForum.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
