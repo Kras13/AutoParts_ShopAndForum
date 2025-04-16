8920bc6aa40e0e29e9e5777f7ab0e6821eb2fbe8
KRASEN <paskalev.1989@abv.bg>
2025-04-16 21:50:20 +0300

    Start of implementing the "available staff" logic.

--- AutoParts_ShopAndForum/AutoParts_ShopAndForum/Hub/ChatHub.cs
+++ AutoParts_ShopAndForum/AutoParts_ShopAndForum/Hub/ChatHub.cs
@@ -1,9 +1,60 @@
 namespace AutoParts_ShopAndForum.Hub;
 
+using System;
+using AutoParts_ShopAndForum.Infrastructure;
 using Microsoft.AspNetCore.SignalR;
 
 public class ChatHub : Hub
 {
+    private static readonly Dictionary<string, (string UserId, bool IsAvailable)> ConnectedSellers = new();
+
+    public override Task OnConnectedAsync()
+    {
+        if (!Context.User.IsAdmin() && !Context.User.IsSeller())
+        {
+            BroadcastUserList();
+
+            return base.OnConnectedAsync();
+        }
+
+        var userId = Context.User.GetId();
+
+        lock (ConnectedSellers)
+        {
+            ConnectedSellers[Context.ConnectionId] = (userId, true);
+        }
+
+        BroadcastUserList();
+
+        return base.OnConnectedAsync();
+    }
+
+    public override Task OnDisconnectedAsync(Exception exception)
+    {
+        if (!Context.User.IsAdmin() && !Context.User.IsSeller())
+            return base.OnDisconnectedAsync(exception);
+
+        lock (ConnectedSellers)
+        {
+            ConnectedSellers.Remove(Context.ConnectionId);
+        }
+
+        BroadcastUserList();
+
+        return base.OnDisconnectedAsync(exception);
+    }
+
+    private Task BroadcastUserList()
+    {
+        var availableUsers = ConnectedSellers
+            .Where(x => x.Value.IsAvailable)
+            .Select(x => x.Value.UserId)
+            .Distinct() // one user might have multiple connections...must appear once
+            .ToList();
+
+        return Clients.All.SendAsync("UpdateUserList", availableUsers);
+    }
+
     public async Task SendMessage(string senderId, string message)
     {
         await Clients.All.SendAsync("ReceiveMessage", senderId, message);
