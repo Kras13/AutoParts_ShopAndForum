72a8e996c79d67a17e40a48ea43d89565af398e0
Kras13 <paskalev.1989@abv.bg>
2025-07-30 22:57:22 +0300

    More fields to edit in Profile section.

--- AutoParts_ShopAndForum/AutoParts_ShopAndForum/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ AutoParts_ShopAndForum/AutoParts_ShopAndForum/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -6,6 +6,8 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using AutoParts_ShopAndForum.Core.Contracts;
+using AutoParts_ShopAndForum.Core.Models.Town;
 using AutoParts_ShopAndForum.Infrastructure.Data.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -17,48 +19,42 @@ namespace AutoParts_ShopAndForum.Areas.Identity.Pages.Account.Manage
     {
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
+        private readonly ITownService _townService;
 
         public IndexModel(
             UserManager<User> userManager,
-            SignInManager<User> signInManager)
+            SignInManager<User> signInManager,
+            ITownService townService)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _townService = townService;
         }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         public string Username { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         [TempData]
         public string StatusMessage { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         [BindProperty]
         public InputModel Input { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         public class InputModel
         {
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [Phone]
             [Display(Name = "Phone number")]
             public string PhoneNumber { get; set; }
+
+            [Display(Name = "Firstname")]
+            public string FirstName { get; set; }
+
+            [Display(Name = "Lastname")]
+            public string LastName { get; set; }
+            
+            [Display(Name = "Town")]
+            public int TownId { get; set; }
+
+            public TownModel[] Towns { get; set; }
         }
 
         private async Task LoadAsync(User user)
@@ -70,25 +66,32 @@ namespace AutoParts_ShopAndForum.Areas.Identity.Pages.Account.Manage
 
             Input = new InputModel
             {
-                PhoneNumber = phoneNumber
+                PhoneNumber = phoneNumber,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                TownId = user.TownId ?? -1,
+                Towns = _townService.GetAll(),
             };
         }
 
         public async Task<IActionResult> OnGetAsync()
         {
             var user = await _userManager.GetUserAsync(User);
+            
             if (user == null)
             {
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
 
             await LoadAsync(user);
+            
             return Page();
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
             var user = await _userManager.GetUserAsync(User);
+            
             if (user == null)
             {
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
@@ -97,22 +100,33 @@ namespace AutoParts_ShopAndForum.Areas.Identity.Pages.Account.Manage
             if (!ModelState.IsValid)
             {
                 await LoadAsync(user);
+                
                 return Page();
             }
 
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+            
             if (Input.PhoneNumber != phoneNumber)
             {
                 var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
+                
                 if (!setPhoneResult.Succeeded)
                 {
                     StatusMessage = "Unexpected error when trying to set phone number.";
+                    
                     return RedirectToPage();
                 }
             }
-
+            
+            user.FirstName = Input.FirstName;
+            user.LastName = Input.LastName;
+            user.TownId = Input.TownId > 0 ? Input.TownId : null;
+            
+            await _userManager.UpdateAsync(user);
             await _signInManager.RefreshSignInAsync(user);
+            
             StatusMessage = "Your profile has been updated";
+            
             return RedirectToPage();
         }
     }
