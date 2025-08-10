7e2ee19dcfb025a68aa192cce10df5e23f49d999
Kras13 <paskalev.1989@abv.bg>
2025-08-10 16:55:45 +0300

    Concurrency fixes in chat hub.

--- AutoParts_ShopAndForum/AutoParts_ShopAndForum/Hub/ChatHub.cs
+++ AutoParts_ShopAndForum/AutoParts_ShopAndForum/Hub/ChatHub.cs
@@ -10,40 +10,48 @@ public class ChatHub : Hub
 {
     private static readonly ConcurrentDictionary<string, ChatUser> UserConnections = new();
     private static readonly ConcurrentDictionary<string, string> ReservedSellers = new();
-    
+    private static readonly object ReservedSellersLock = new();
+    private static readonly object UserConnectionsLock = new();
+
     public override Task OnConnectedAsync()
     {
         var userId = Context.User.GetId();
         var isSeller = Context.User.IsSeller() || Context.User.IsAdmin();
-            
+
         UserConnections[Context.ConnectionId] = new ChatUser(
             userId, Context.User.GetEmail(), isSeller);
-    
+
         BroadcastSellersList();
-        
+
         return base.OnConnectedAsync();
     }
-    
+
     public override Task OnDisconnectedAsync(Exception exception)
     {
         UserConnections.Remove(Context.ConnectionId, out var chatUser);
 
         if (chatUser == null)
             return base.OnDisconnectedAsync(exception);
-        
-        if (chatUser.IsSeller)
+
+        lock (ReservedSellersLock)
         {
-            ReservedSellers.TryRemove(chatUser.Id, out _);
+            if (chatUser.IsSeller)
+            {
+                ReservedSellers.TryRemove(chatUser.Id, out _);
+            }
+            else
+            {
+                var sellerId = ReservedSellers.FirstOrDefault(kvp => kvp.Value == chatUser.Id).Key;
+
+                if (sellerId != null)
+                {
+                    ReservedSellers.TryRemove(sellerId, out _);
+                }
+            }
         }
-        else
-        {
-            var sellerId = ReservedSellers.FirstOrDefault(kvp => kvp.Value == chatUser.Id).Key;
-            
-            ReservedSellers.TryRemove(sellerId, out _);
-        }
-    
+
         BroadcastSellersList();
-    
+
         return base.OnDisconnectedAsync(exception);
     }
 
@@ -60,33 +68,39 @@ public class ChatHub : Hub
 
     public bool StartPrivateChat(string initiatorId, string sellerId)
     {
-        if (ReservedSellers.ContainsKey(sellerId))
-            return false;
-        
-        ReservedSellers.TryAdd(sellerId, initiatorId);
+        var result = ReservedSellers.TryAdd(sellerId, initiatorId);
 
-        BroadcastSellersList();
+        if (result)
+            BroadcastSellersList();
 
-        return true; // todo - think if the sellers to be kept in one connection only (bad if multiple tabs opened)
+        return result;
     }
-    
+
     public bool EndPrivateChat(string sellerId)
     {
         var result = ReservedSellers.TryRemove(sellerId, out _);
 
         BroadcastSellersList();
-        
+
         return result;
     }
-    
-    public async Task SendPrivateMessage(string senderId, string receiverId, string message)
+
+    public async Task SendPrivateMessage(string receiverId, string message)
     {
-        var receiverConnections = UserConnections
-            .Where(x => x.Value.Id == receiverId)
-            .Select(x => x.Key);
-        
-        var senderEmail = UserConnections.FirstOrDefault(x => x.Value.Id == senderId).Value.Email;
-        
-        await Clients.Clients(receiverConnections).SendAsync("ReceivePrivateMessage", senderId, senderEmail, message);
+        IEnumerable<string> receiverConnections;
+        ChatUser sender;
+
+        lock (UserConnectionsLock)
+        {
+            receiverConnections = UserConnections
+                .Where(x => x.Value.Id == receiverId)
+                .Select(x => x.Key)
+                .ToList();
+
+            if (!UserConnections.TryGetValue(Context.ConnectionId, out sender))
+                return;
+        }
+
+        await Clients.Clients(receiverConnections).SendAsync("ReceivePrivateMessage", sender.Id, sender.Email, message);
     }
 }
